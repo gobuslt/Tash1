b8121360fad57f1d4b68c46d17b4da8b3899bcd6
gobuslt <gobuslt07@gmail.com>
2023-02-01 13:47:57 +0530

    Java

---
+++ src/org/test/GitTool.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class GitTool {
+
+	public void demo1() {
+System.out.println("Demo project 1");
+	}
+	public void demo2() {
+System.out.println("Demo project 2");
+	}
+
+	public void demo3() {
+System.out.println("Demo project 3");
+	}
+
+	
+}
